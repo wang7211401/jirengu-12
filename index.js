bcf46de3318019bd814881e704e226b8d808f324
student217 <student217@iwritejs.com>
2017-05-04 10:58:28 +0800

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var fs = require('fs')
 var url = require('url')
 
 //console.log(Object.keys(http))
-var port = process.env.PORT || 9527;
+var port = process.env.PORT || 9555;
 
 var server = http.createServer(function(request, response){
 
